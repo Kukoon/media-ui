89e0072495581f6c727b1bf7df491304ef29985e
janebuoy <janebuoy@posteo.de>
2021-03-06 11:13:05 +0100

    Add transition between poster and webp preview

--- src/components/Poster.vue
+++ src/components/Poster.vue
@@ -5,53 +5,52 @@
     :title="video.lang.title"
     @click.stop="goToTop()"
   >
-    <v-hover>
-      <template v-slot:default="{ hover }">
-        <div
-          style="height: 100%"
-          id="switcherWrapper"
-          @mouseenter="showPreview = true"
-          @mouseleave="showPreview = false"
+    <div
+      style="height: 100%; position: relative"
+      id="switcherWrapper"
+      @mouseenter="showPreview = true"
+      @mouseleave="showPreview = false"
+    >
+      <v-img
+        v-ripple="{ class: 'neutral--text', center: true }"
+        :src="preview"
+        class="white--text align-end"
+        gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.5)"
+        height="100%"
+        width="100%"
+        style="position: absolute"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <transition name="fade" appear>
+        <v-img
+          v-if="!showPreview"
+          v-ripple="{ class: 'neutral--text', center: true }"
+          :src="source"
+          class="white--text align-end"
+          gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.5)"
+          height="100%"
+          width="100%"
+          style="position: absolute"
         >
-          <v-img
-            v-if="!showPreview && !hover"
-            v-ripple="{ class: 'neutral--text', center: true }"
-            :src="source"
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.5)"
-            height="100%"
-            width="100%"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-          <v-img
-            v-else
-            v-ripple="{ class: 'neutral--text', center: true }"
-            :src="preview"
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.5)"
-            height="100%"
-            width="100%"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </div>
-      </template>
-    </v-hover>
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </transition>
+    </div>
   </router-link>
 </template>
 
@@ -84,5 +83,12 @@ export default {
 }
 .pointer {
   cursor: pointer;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
