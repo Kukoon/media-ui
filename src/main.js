3cfb5ca4d23d6696cd68b2a9b8f7d0863e1d35ee
Geno <geno+dev@fireorbit.de>
2021-07-25 22:11:56 +0200

    depends: self-host 3rd parties

--- src/main.js
+++ src/main.js
@@ -4,7 +4,23 @@ import vuetify from './plugins/vuetify';
 import VueChatScroll from 'vue-chat-scroll';
 import router from './router'
 
-import VueMasonry from 'vue-masonry-css'
+import VueMasonry from 'vue-masonry-css';
+
+import '@fontsource/fira-sans/index.css';
+import "@fontsource/fira-sans/100.css";
+import "@fontsource/fira-sans/300.css";
+import "@fontsource/fira-sans/400.css";
+import "@fontsource/fira-sans/500.css";
+import "@fontsource/fira-sans/700.css";
+import "@fontsource/fira-sans/900.css";
+import '@fontsource/overpass/index.css';
+import "@fontsource/overpass/100.css";
+import "@fontsource/overpass/300.css";
+import "@fontsource/overpass/400.css";
+import "@fontsource/overpass/700.css";
+import "@fontsource/overpass/900.css";
+import '@mdi/font/css/materialdesignicons.min.css';
+
 
 Vue.config.productionTip = false
 
