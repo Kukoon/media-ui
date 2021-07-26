50cf579067512d597158b0a30475c90742659489
Geno <geno+dev@fireorbit.de>
2021-07-26 17:49:48 +0200

    Add Download Button

--- src/components/VideoDescription.vue
+++ src/components/VideoDescription.vue
@@ -1,20 +1,10 @@
 <template>
   <div>
-    <!-- Show Tags -->
-    <div v-if="tagsPosition === 'top'">
-      <Speakers :speakers="video.speakers" />
-      <Tags
-        :tags="video.tags"
-        :event="video.event"
-        :duration="video.duration"
-        :running="video.running"
-        :viewers="video.viewers"
-      />
-    </div>
-    <v-card-text class="px-0" v-if="markedDesc">
+    <v-card-text class="px-0" v-if="markedDesc && tagsPosition !== 'top'">
       <td v-html="markedDesc"></td>
     </v-card-text>
-    <div v-if="tagsPosition === 'bottom'">
+    <div v-if="tagsPosition">
+      <BTNVideoDownload :sources="video.formats"/>
       <Speakers :speakers="video.speakers" />
       <Tags
         :tags="video.tags"
@@ -24,18 +14,22 @@
         :viewers="video.viewers"
       />
     </div>
+    <v-card-text class="px-0" v-if="markedDesc && tagsPosition === 'top'">
+      <td v-html="markedDesc"></td>
+    </v-card-text>
   </div>
 </template>
 
 <script>
 import marked from "marked";
 
+import BTNVideoDownload from "@/components/BTNVideoDownload.vue";
 import Tags from "@/components/Tags.vue";
 import Speakers from "@/components/Speakers.vue";
 
 export default {
   name: "VideoDescription",
-  components: { Tags, Speakers },
+  components: { BTNVideoDownload, Tags, Speakers },
   props: ["tagsPosition", "video", "dense", "long"],
   computed: {
     markedDesc() {
