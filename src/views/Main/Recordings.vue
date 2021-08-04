d7bc49106955195f6f88a3306d91b891337cc175
Geno <geno+dev@fireorbit.de>
2021-08-04 20:54:22 +0200

    filter of recordings

--- src/views/Main/Recordings.vue
+++ src/views/Main/Recordings.vue
@@ -71,6 +71,52 @@
         </v-btn-toggle>
       </v-col>
     </v-row>
+    <v-row no-gutters class="d-flex align-center mx-2 mb-3">
+      <v-col cols="4">
+        <v-autocomplete
+          label="Events"
+          :items="events"
+          item-text="name"
+          item-value="id"
+          v-model="eventsFilter"
+          dense
+          chips
+          small-chips
+          clearable
+         >
+        </v-autocomplete>
+      </v-col>
+      <v-col cols="4">
+        <v-autocomplete
+          label="Tags"
+          :items="tags"
+          item-text="lang.name"
+          item-value="id"
+          v-model="tagsFilter"
+          dense
+          chips
+          small-chips
+          deletable-chips
+          multiple
+         >
+        </v-autocomplete>
+      </v-col>
+      <v-col cols="4">
+        <v-autocomplete
+          label="Speakers"
+          :items="speakers"
+          item-text="name"
+          item-value="id"
+          v-model="speakersFilter"
+          dense
+          chips
+          small-chips
+          deletable-chips
+          multiple
+         >
+        </v-autocomplete>
+      </v-col>
+    </v-row>
     <router-view :videos="recordings"></router-view>
   </v-container>
 </template>
@@ -86,6 +132,9 @@ export default {
       audioPodcastSrc: "https://media.kukoon.de/rss/kukoon/de/audio_best",
       videoPodcastSrc: "https://media.kukoon.de/rss/kukoon/de/video_best",
       recordings: [],
+      tags: [],
+      speakers: [],
+      events: [],
     };
   },
   computed: {
@@ -101,8 +150,45 @@ export default {
         .map((el) => el.channel);
       return channels;
     },
+    tagsFilter: {
+      cache: false,
+      get() { return this.filterGet("tag")},
+      set(newValue) { this.filterSet("tag", newValue)},
+    },
+    speakersFilter: {
+      cache: false,
+      get() { return this.filterGet("speaker")},
+      set(newValue) { this.filterSet("speaker", newValue)},
+    },
+    eventsFilter: {
+      cache: false,
+      get() { return this.filterGet("event")},
+      set(newValue) { this.filterSet("event", newValue)},
+    },
   },
   methods: {
+    filterGet(key) {
+        return this.$router.history.current.query[key] ? this.$router.history.current.query[key] : [];
+    },
+    filterSet(key, value) {
+	var query = Object.assign({}, this.$router.history.current.query);
+        if (value) {
+          query[key] = value;
+        } else {
+          delete query[key]
+        }
+        console.log("from", this.$router.history.current, "to", query);
+        this.$router.push({
+          name: this.$router.history.current.name,
+          query: query,
+        });
+      
+    },
+    loadFilterData() {
+      api.ListTags().then((response) => (this.tags = response.data))
+      api.ListEvents().then((response) => (this.events = response.data))
+      api.ListSpeakers().then((response) => (this.speakers = response.data))
+    },
     load() {
       api.ListRecordings(this.$router.history.current.query).then((response) => (this.recordings = response.data))
     },
@@ -111,9 +197,13 @@ export default {
   watch: {
     $route() {
       this.load();
+      this.$forceCmpile('tagsFilter');
+      this.$forceCmpile('speakersFilter');
+      this.$forceCmpile('eventsFilter');
     },
   },
   created() {
+    this.loadFilterData();
     this.load();
   },
 };
