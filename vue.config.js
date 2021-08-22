2375a5c0acbf28fa969c70249a2bf001ec99463f
Geno <geno+dev@fireorbit.de>
2021-08-22 12:51:41 +0200

    move API for developing into proxy of vuejs

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,20 @@ module.exports = {
 	// },
 	transpileDependencies: [
 		"vuetify"
-	]
+	],
+	devServer: {
+		proxy: {
+			'^/api': {
+				target: 'http://localhost:8090',
+				// target: 'https://v2-dev.media.kukoon.de',
+				// target: 'https://media.kukoon.de',
+				changeOrigin: true,
+			},
+			'^/ws': {
+				target: 'https://media.kukoon.de',
+				ws: true,
+				changeOrigin: true,
+			},
+		},
+	},
 }
